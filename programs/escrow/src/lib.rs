3bb212d4a34c140c4e0c2a434c36c506a9308329
Mike Olson <mike@madtownmining.com>
2025-07-06 22:43:24 -0500

    Initial escrow commit.

---
+++ programs/escrow/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("ECyZLEn8w2Xoitt6pRyZ1K6AvkJYdrpzcyjctY39CobE");
+
+#[program]
+pub mod escrow {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
